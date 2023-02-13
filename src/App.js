127ef2102784eb7a16c822fade00127d4ddc56d2
Arshvir Mundi <arshvir.mundi@gmail.com>
2023-02-12 21:42:52 -0500

    added body folder

--- src/App.js
+++ src/App.js
@@ -1,44 +1,10 @@
 import "./App.scss";
-import JSONDATA from "./countries-data.json";
-import logo from "./logo.png";
-
-import { useState } from "react";
+import Body from "./body/body.jsx";
 
 function App() {
-  const [searchTerm, setSearchTerm] = useState("");
-
   return (
     <div className="App">
-      <div className="navbar">
-        <img src={logo} className="logo" />
-        <input
-          className="searchBox"
-          type="text"
-          placeholder="Search..."
-          onChange={(e) => {
-            setSearchTerm(e.target.value);
-          }}
-        />
-      </div>
-
-      <div className="country-box">
-        {JSONDATA.filter((value) => {
-          if (searchTerm === "") {
-            return value;
-          } else if (
-            value.country.toLowerCase().includes(searchTerm.toLowerCase())
-          ) {
-            return value;
-          }
-        }).map((value, key) => {
-          return (
-            <div className="country" key={key}>
-              <img src={value.flag_base64} className="flagImage" />
-              <p>{value.country}</p>
-            </div>
-          );
-        })}
-      </div>
+      <Body />
     </div>
   );
 }
